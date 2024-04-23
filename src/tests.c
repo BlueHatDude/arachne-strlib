a0d868587594a64f27dc84b57bad4fa3fd7ed979
Blue Hat Aspie <ehmdiagne0530@gmail.com>
2024-04-23 11:41:53 -0400

    updated README.md

--- src/tests.c
+++ src/tests.c
@@ -4,7 +4,8 @@
 
 int main(void) {
     A_String s1;
-    A_String_setAndInit(&s1, 25, "Hello ");
+    A_String_init(&s1, 25);
+    A_String_setStr(&s1, "Hello ");
 
     A_String s2;
     A_String_setAndInit(&s2, 15, "World");
@@ -12,4 +13,12 @@ int main(void) {
     A_String_catStr(&s1, &s2);
     printf("%s \n", A_String_getStr(&s1));
 
+    A_String s3;
+    A_String_setAndInit(&s3, 20, "AAAAAAAAAAAAAAAAAAAA");
+
+    A_String_catStr(&s1, &s3);
+    printf("%s \n", A_String_getStr(&s1));
+
+    A_String_deinit(&s1);
+    A_String_deinit(&s2);    
 }
